122ae41b875e0b7c0f381d05e8d0154ad045c882
diljitsgit <diljits1776@gmail.com>
2024-12-21 19:14:07 +0530

    UPDATE tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-    theme: {
-        extend: {
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            colors: {
-                dbackground: "#090910",
-                lbackground: "#D8D8D8",
-                grayTxt: "#747474",
-                primary: "#3E5EFF",
-                outline: "#27272A",
-            },
-            fontFamily: {
-                logo: ["logo-font"],
-                info: ["logo-info"],
-            },
+export const darkMode = ["class"];
+export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
+export const theme = {
+    extend: {
+        borderRadius: {
+            lg: "var(--radius)",
+            md: "calc(var(--radius) - 2px)",
+            sm: "calc(var(--radius) - 4px)",
+        },
+        colors: {
+            dbackground: "#090910",
+            lbackground: "#D8D8D8",
+            grayTxt: "#747474",
+            primary: "#3E5EFF",
+            outline: "#27272A",
+        },
+        fontFamily: {
+            logo: ["logo-font"],
+            info: ["logo-info"],
         },
     },
-    plugins: [require("tailwindcss-animate")],
 };
+export const plugins = [require("tailwindcss-animate")];
