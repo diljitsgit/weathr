eee2bf5f914ff637e28f2cd9507607743128a4a4
diljitsgit <diljits1776@gmail.com>
2024-12-28 02:55:12 +0530

    Creating file structure and shifting approprate content to files

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,227 +1,28 @@
-import { fetchWeatherApi } from "openmeteo";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Component from "./chart";
 import { ChangeEvent, useEffect, useState } from "react";
-
-let permissionGiven = false;
-
-type apiData = {
-    current: {
-        time: Date;
-        temperature2m: number;
-        relativeHumidity2m: number;
-        precipitation: number;
-        weatherCode: number;
-        windSpeed10m: number;
-    };
-    hourly: {
-        time: Date[];
-        temperature2m: Float32Array;
-        precipitation: Float32Array;
-        weatherCode: Float32Array;
-        windSpeed10m: Float32Array;
-        windDirection10m: Float32Array;
-    };
-    daily: {
-        weatherCode: Float32Array;
-        temperature2mMax: Float32Array;
-        temperature2mMin: Float32Array;
-    };
-} | null;
-
-type locationData = {
-    admin1: string;
-    admin1_id: number;
-    admin3: string;
-    admin3_id: number;
-    admin4: string;
-    admin4_id: number;
-    country: string;
-    country_code: string;
-    country_id: number;
-    elevation: number;
-    feature_code: string;
-    id: number;
-    latitude: number;
-    longitude: number;
-    name: string;
-    population: number;
-    postcodes: number[];
-    timezone: string;
-};
-
-type suggestionsData = {
-    results: Array<locationData>;
-    generationtime_ms: number;
-} | null;
-
-const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-
-async function getWeatherData(lat: number, long: number) {
-    const params = {
-        latitude: lat,
-        longitude: long,
-        current: [
-            "temperature_2m",
-            "relative_humidity_2m",
-            "precipitation",
-            "weather_code",
-            "wind_speed_10m",
-        ],
-        hourly: [
-            "temperature_2m",
-            "precipitation",
-            "weather_code",
-            "wind_speed_10m",
-            "wind_direction_10m",
-        ],
-        daily: ["weather_code", "temperature_2m_max", "temperature_2m_min"],
-    };
-    const url = "https://api.open-meteo.com/v1/forecast";
-    const responses = await fetchWeatherApi(url, params);
-
-    // Helper function to form time ranges
-    const range = (start: number, stop: number, step: number) =>
-        Array.from(
-            { length: (stop - start) / step },
-            (_, i) => start + i * step
-        );
-
-    // Process first location. Add a for-loop for multiple locations or weather models
-    const response = responses[0];
-
-    // Attributes for timezone and location
-    const utcOffsetSeconds = response.utcOffsetSeconds();
-
-    const current = response.current()!;
-    const hourly = response.hourly()!;
-    const daily = response.daily()!;
-
-    // Note: The order of weather variables in the URL query and the indices below need to match!
-    const weatherData = {
-        current: {
-            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-            temperature2m: current.variables(0)!.value(),
-            relativeHumidity2m: current.variables(1)!.value(),
-            precipitation: current.variables(2)!.value(),
-            weatherCode: current.variables(3)!.value(),
-            windSpeed10m: current.variables(4)!.value(),
-        },
-        hourly: {
-            time: range(
-                Number(hourly.time()),
-                Number(hourly.timeEnd()),
-                hourly.interval()
-            ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
-            temperature2m: hourly.variables(0)!.valuesArray()!,
-            precipitation: hourly.variables(1)!.valuesArray()!,
-            weatherCode: hourly.variables(2)!.valuesArray()!,
-            windSpeed10m: hourly.variables(3)!.valuesArray()!,
-            windDirection10m: hourly.variables(4)!.valuesArray()!,
-        },
-        daily: {
-            time: range(
-                Number(daily.time()),
-                Number(daily.timeEnd()),
-                daily.interval()
-            ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
-            weatherCode: daily.variables(0)!.valuesArray()!,
-            temperature2mMax: daily.variables(1)!.valuesArray()!,
-            temperature2mMin: daily.variables(2)!.valuesArray()!,
-        },
-    };
-
-    return weatherData;
-}
-
-async function getLocationData(location: string) {
-    const url =
-        "https://geocoding-api.open-meteo.com/v1/search?name=" +
-        location +
-        "&count=7";
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = response.json();
-
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-function weatherCodeConversion(a: number): string {
-    if (a >= 0 && a <= 3) {
-        return "Cloudy";
-    } else if ((a >= 4 && a <= 12) || a == 28 || (a >= 40 && a <= 49)) {
-        return "Fog";
-    } else if (a == 13) {
-        return "Rain-ThunderStorm";
-    } else if (a >= 15 && a <= 16) {
-        return "Humidity";
-    } else if ((a >= 17 && a <= 19) || a == 29 || (a >= 95 && a <= 99)) {
-        return "Severe-ThunderStorm";
-    } else if ((a >= 50 && a <= 59) || a == 20) {
-        return "Drizzle";
-    } else if (
-        (a >= 24 && a <= 25) ||
-        a == 21 ||
-        (a >= 60 && a <= 69) ||
-        (a >= 80 && a <= 84)
-    ) {
-        return "Rain";
-    } else if (a == 26 || (a >= 70 && a <= 79) || (a >= 85 && a <= 86)) {
-        return "Snow";
-    } else if (a == 27 || (a >= 87 && a <= 94)) {
-        return "Hail";
-    } else if (a >= 30 && a <= 35) {
-        return "Wind";
-    } else if (a >= 36 && a <= 39) {
-        return "Blowing-Snow";
-    }
-
-    return "Unknown";
-}
-
-const useUserCoords = () => {
-    const [latitude, setLatitude] = useState(0);
-    const [longitude, setLongitude] = useState(0);
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-        });
-    }
-
-    if (latitude != 0 && longitude != 0) {
-        return {
-            lat: latitude,
-            long: longitude,
-        };
-    }
-    return null;
-};
+import { apiData, suggestionsData } from "./lib/types";
+import { WEEKDAY } from "./lib/constants";
+import { useUserCoords } from "./lib/hooks";
+import { getWeatherData } from "./lib/fetchWeatherData";
+import {
+    presChartData,
+    tempChartData,
+    weatherCodeConversion,
+    windChartData,
+} from "./lib/utils";
+import { getLocationData } from "./lib/axios";
+import Daily from "./components/ui/DailyData";
+import Suggestions from "./components/ui/LocationSuggestions";
 
 function Weather() {
     const userCoords = useUserCoords();
     const [lat, setLat] = useState(0);
     const [long, setLong] = useState(0);
-    const range = 24;
     const [weatherData, setWeatherData] = useState<apiData>(null);
     const [location, setLocation] = useState("");
     const [locationData, setLocationData] = useState<suggestionsData>(null);
+    const [permissionGiven, setPermissionGiven] = useState(false);
 
     const handleLocationChange = (e: ChangeEvent<HTMLInputElement>) => {
         setLocation(e.target.value);
@@ -256,13 +57,13 @@ function Weather() {
 
     const handleClick = () => {
         setLocation("");
-        permissionGiven = true;
+        setPermissionGiven(true);
     };
 
     function handleData(data: apiData, lat: number, long: number) {
         if (lat != 0 && long != 0) {
             setWeatherData(data);
-            permissionGiven = true;
+            setPermissionGiven(true);
         }
     }
 
@@ -280,60 +81,6 @@ function Weather() {
 
         return;
     }, [lat, long]);
-
-    const tempChartData = () => {
-        const out = [];
-        if (weatherData) {
-            for (
-                let i = weatherData.current.time.getHours();
-                i < weatherData.current.time.getHours() + range;
-                i++
-            ) {
-                const temp = {
-                    time: (i % 24) + ":00",
-                    temp: Math.floor(weatherData.hourly.temperature2m[i]),
-                };
-                out.push(temp);
-            }
-        }
-        return out;
-    };
-
-    const presChartData = () => {
-        const out = [];
-        if (weatherData) {
-            for (
-                let i = weatherData.current.time.getHours();
-                i < weatherData.current.time.getHours() + range;
-                i++
-            ) {
-                const temp = {
-                    time: (i % 24) + ":00",
-                    temp: Math.floor(weatherData.hourly.precipitation[i]),
-                };
-                out.push(temp);
-            }
-        }
-        return out;
-    };
-
-    const windChartData = () => {
-        const out = [];
-        if (weatherData) {
-            for (
-                let i = weatherData.current.time.getHours();
-                i < weatherData.current.time.getHours() + range;
-                i++
-            ) {
-                const temp = {
-                    time: (i % 24) + ":00",
-                    temp: Math.floor(weatherData.hourly.windSpeed10m[i]),
-                };
-                out.push(temp);
-            }
-        }
-        return out;
-    };
 
     return (
         <>
@@ -369,7 +116,7 @@ function Weather() {
                                     <div className="flex gap-3 items-baseline">
                                         <img
                                             src={
-                                                "/src/assets/new-icons/" +
+                                                "/src/assets/weather-icons/" +
                                                 weatherCodeConversion(
                                                     weatherData.current
                                                         .weatherCode
@@ -394,7 +141,7 @@ function Weather() {
                                     </h4>
                                     <p className="text-grayTxt text-sm leading-3">
                                         {
-                                            weekday[
+                                            WEEKDAY[
                                                 weatherData.current.time.getDay()
                                             ]
                                         }
@@ -423,7 +170,9 @@ function Weather() {
                                 <TabsContent value="temprature">
                                     <div className="my-6">
                                         <Component
-                                            chartData={tempChartData()}
+                                            chartData={tempChartData(
+                                                weatherData
+                                            )}
                                             chartColor={"#3E5EFF"}
                                         ></Component>
                                     </div>
@@ -431,7 +180,9 @@ function Weather() {
                                 <TabsContent value="precipatation">
                                     <div className="my-6">
                                         <Component
-                                            chartData={presChartData()}
+                                            chartData={presChartData(
+                                                weatherData
+                                            )}
                                             chartColor={"#E59061"}
                                         ></Component>
                                     </div>
@@ -439,7 +190,9 @@ function Weather() {
                                 <TabsContent value="wind">
                                     <div className="my-6">
                                         <Component
-                                            chartData={windChartData()}
+                                            chartData={windChartData(
+                                                weatherData
+                                            )}
                                             chartColor={"#95B6F6"}
                                         ></Component>
                                     </div>
@@ -483,94 +236,6 @@ function Weather() {
             )}
         </>
     );
-}
-
-function Daily({ weatherData }: { weatherData: apiData }) {
-    const dailyData = () => {
-        const out = [];
-        if (weatherData) {
-            for (let i = 0; i < 7; i++) {
-                const temp = {
-                    day: weekday[(weatherData.current.time.getDate() + i) % 7],
-                    weatherCode: weatherData.daily.weatherCode[i],
-                    minTemp: weatherData.daily.temperature2mMin[i],
-                    maxTemp: weatherData.daily.temperature2mMax[i],
-                };
-                out.push(temp);
-            }
-        }
-        return out;
-    };
-
-    return (
-        <>
-            {dailyData().map((a) => {
-                return (
-                    <div
-                        key={a.day}
-                        className="flex flex-col min-w-max border-[1px] border-outline rounded-xl lg:rounded-3xl md:rounded-2xl justify-between items-center px-3 py-2"
-                    >
-                        <p className="text-lg text-black dark:text-white">
-                            {a.day.slice(0, 3)}
-                        </p>
-                        <img
-                            src={
-                                "/src/assets/new-icons/" +
-                                weatherCodeConversion(a.weatherCode) +
-                                ".svg"
-                            }
-                            alt="There should be a icon here"
-                            className="size-12 mb-2"
-                        />
-                        <div>
-                            <p className="text-sm text-grayTxt leading-3">
-                                {Math.floor(a.maxTemp)}°
-                            </p>
-                            <p className="text-sm text-grayTxt">
-                                {Math.floor(a.minTemp)}°
-                            </p>
-                        </div>
-                    </div>
-                );
-            })}
-        </>
-    );
-}
-
-function Suggestions({
-    data,
-    functionDrill,
-}: {
-    data: suggestionsData;
-    functionDrill: (lat: number, long: number) => void;
-}) {
-    if (data) {
-        if (data.results) {
-            return (
-                <>
-                    <div className="absolute divide-y divide-outline border-[1px] z-10 border-outline rounded-xl lg:rounded-3xl md:rounded-2xl min-w-max dark:bg-dbackground bg-lbackground">
-                        {data.results.map((a, index) => {
-                            return (
-                                <div
-                                    className="px-4 py-2 "
-                                    key={index}
-                                    onClick={() => {
-                                        functionDrill(a.latitude, a.longitude);
-                                    }}
-                                >
-                                    {a.name},{" "}
-                                    <span className="text-grayTxt text-sm">
-                                        {a.admin1}, {a.country}
-                                    </span>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </>
-            );
-        }
-    }
-    return <></>;
 }
 
 export default Weather;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,95 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { RANGE } from "./constants";
+import { apiData } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export function weatherCodeConversion(a: number): string {
+    if (a >= 0 && a <= 3) {
+        return "Cloudy";
+    } else if ((a >= 4 && a <= 12) || a == 28 || (a >= 40 && a <= 49)) {
+        return "Fog";
+    } else if (a == 13) {
+        return "Rain-ThunderStorm";
+    } else if (a >= 15 && a <= 16) {
+        return "Humidity";
+    } else if ((a >= 17 && a <= 19) || a == 29 || (a >= 95 && a <= 99)) {
+        return "Severe-ThunderStorm";
+    } else if ((a >= 50 && a <= 59) || a == 20) {
+        return "Drizzle";
+    } else if (
+        (a >= 24 && a <= 25) ||
+        a == 21 ||
+        (a >= 60 && a <= 69) ||
+        (a >= 80 && a <= 84)
+    ) {
+        return "Rain";
+    } else if (a == 26 || (a >= 70 && a <= 79) || (a >= 85 && a <= 86)) {
+        return "Snow";
+    } else if (a == 27 || (a >= 87 && a <= 94)) {
+        return "Hail";
+    } else if (a >= 30 && a <= 35) {
+        return "Wind";
+    } else if (a >= 36 && a <= 39) {
+        return "Blowing-Snow";
+    }
+
+    return "Unknown";
+}
+
+export const tempChartData = (weatherData: apiData) => {
+    const out = [];
+    if (weatherData) {
+        for (
+            let i = weatherData.current.time.getHours();
+            i < weatherData.current.time.getHours() + RANGE;
+            i++
+        ) {
+            const temp = {
+                time: (i % 24) + ":00",
+                temp: Math.floor(weatherData.hourly.temperature2m[i]),
+            };
+            out.push(temp);
+        }
+    }
+    return out;
+};
+
+export const presChartData = (weatherData: apiData) => {
+    const out = [];
+    if (weatherData) {
+        for (
+            let i = weatherData.current.time.getHours();
+            i < weatherData.current.time.getHours() + RANGE;
+            i++
+        ) {
+            const temp = {
+                time: (i % 24) + ":00",
+                temp: Math.floor(weatherData.hourly.precipitation[i]),
+            };
+            out.push(temp);
+        }
+    }
+    return out;
+};
+
+export const windChartData = (weatherData: apiData) => {
+    const out = [];
+    if (weatherData) {
+        for (
+            let i = weatherData.current.time.getHours();
+            i < weatherData.current.time.getHours() + RANGE;
+            i++
+        ) {
+            const temp = {
+                time: (i % 24) + ":00",
+                temp: Math.floor(weatherData.hourly.windSpeed10m[i]),
+            };
+            out.push(temp);
+        }
+    }
+    return out;
+};
