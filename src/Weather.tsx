f2cbe7bc8d5c45449051b5425ffc6d12dad2a0d3
diljitsgit <diljits1776@gmail.com>
2024-12-28 19:08:48 +0530

    Adding components to clean code

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -2,18 +2,16 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Component from "./chart";
 import { ChangeEvent, useEffect, useState } from "react";
 import { apiData, suggestionsData } from "./lib/types";
-import { WEEKDAY } from "./lib/constants";
 import { useUserCoords } from "./lib/hooks";
 import { getWeatherData } from "./lib/fetchWeatherData";
-import {
-    presChartData,
-    tempChartData,
-    weatherCodeConversion,
-    windChartData,
-} from "./lib/utils";
+import { presChartData, tempChartData, windChartData } from "./lib/utils";
 import { getLocationData } from "./lib/axios";
 import Daily from "./components/ui/DailyData";
 import Suggestions from "./components/ui/LocationSuggestions";
+import Input from "./components/ui/Input";
+import Button from "./components/ui/Button";
+import CurrentTemprature from "./components/ui/CurrentTemprature";
+import CurrentWeather from "./components/ui/CurrentWeather";
 
 function Weather() {
     const userCoords = useUserCoords();
@@ -47,19 +45,17 @@ function Weather() {
         setLong(long);
         setLocation("");
     }
-
     useEffect(() => {
         if (userCoords && !permissionGiven) {
             setLat(userCoords.lat);
             setLong(userCoords.long);
         }
-    }, [userCoords]);
+    }, [permissionGiven, userCoords]);
 
     const handleClick = () => {
         setLocation("");
         setPermissionGiven(true);
     };
-
     function handleData(data: apiData, lat: number, long: number) {
         if (lat != 0 && long != 0) {
             setWeatherData(data);
@@ -86,152 +82,70 @@ function Weather() {
         <>
             {permissionGiven && weatherData ? (
                 <>
-                    <div>
-                        <div>
-                            <div className="flex items-center gap-2 my-2 overflow-x-scroll md:overflow-hidden">
-                                <div>
-                                    <input
-                                        className="px-3 py-1 bg-transparent text-black dark:text-white rounded-md border-[1px] border-outline lg:rounded-xl md:rounded-lg focus:outline-none"
-                                        type="text"
-                                        name="location"
-                                        value={location}
-                                        onChange={handleLocationChange}
-                                        placeholder="enter city..."
-                                    />
-                                    <Suggestions
-                                        data={locationData}
-                                        functionDrill={suggestionSelected}
-                                    ></Suggestions>
-                                </div>
-                                <button
-                                    onClick={handleClick}
-                                    className="px-3 py-1 bg-transparent text-black dark:text-white rounded-md border-[1px] border-outline lg:rounded-xl md:rounded-lg focus:outline-none"
-                                >
-                                    Clear
-                                </button>
-                            </div>
-
-                            <div className="flex justify-between items-center font-info">
-                                <div className="flex items-baseline">
-                                    <div className="flex gap-3 items-baseline">
-                                        <img
-                                            src={
-                                                "/src/assets/weather-icons/" +
-                                                weatherCodeConversion(
-                                                    weatherData.current
-                                                        .weatherCode
-                                                ) +
-                                                ".svg"
-                                            }
-                                            alt="a icon should be here"
-                                            className="size-12"
-                                        />
-                                        <p className="text-[4rem] text-black dark:text-white">
-                                            {Math.floor(
-                                                weatherData.current
-                                                    .temperature2m
-                                            )}
-                                            °
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="text-right flex flex-col">
-                                    <h4 className="text-lg text-black dark:text-white leading-6">
-                                        Weather
-                                    </h4>
-                                    <p className="text-grayTxt text-sm leading-3">
-                                        {
-                                            WEEKDAY[
-                                                weatherData.current.time.getDay()
-                                            ]
-                                        }
-                                        , {weatherData.current.time.getHours()}
-                                        :00
-                                    </p>
-                                    <p className="text-grayTxt text-sm leading-4">
-                                        {weatherCodeConversion(
-                                            weatherData.current.weatherCode
-                                        ).replace("-", " ")}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="block">
-                            <Tabs defaultValue="temprature" className="flex-1">
-                                <TabsList>
-                                    <TabsTrigger value="temprature">
-                                        Temprature
-                                    </TabsTrigger>
-                                    <TabsTrigger value="precipatation">
-                                        Precipitation
-                                    </TabsTrigger>
-                                    <TabsTrigger value="wind">Wind</TabsTrigger>
-                                </TabsList>
-                                <TabsContent value="temprature">
-                                    <div className="my-6">
-                                        <Component
-                                            chartData={tempChartData(
-                                                weatherData
-                                            )}
-                                            chartColor={"#3E5EFF"}
-                                        ></Component>
-                                    </div>
-                                </TabsContent>
-                                <TabsContent value="precipatation">
-                                    <div className="my-6">
-                                        <Component
-                                            chartData={presChartData(
-                                                weatherData
-                                            )}
-                                            chartColor={"#E59061"}
-                                        ></Component>
-                                    </div>
-                                </TabsContent>
-                                <TabsContent value="wind">
-                                    <div className="my-6">
-                                        <Component
-                                            chartData={windChartData(
-                                                weatherData
-                                            )}
-                                            chartColor={"#95B6F6"}
-                                        ></Component>
-                                    </div>
-                                </TabsContent>
-                            </Tabs>
-                            <div className="overflow-scroll md:overflow-hidden">
-                                <div className="flex md:justify-center">
-                                    <div className="flex gap-2">
-                                        <Daily
-                                            weatherData={weatherData}
-                                        ></Daily>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <div className="flex items-center gap-2 my-2 overflow-x-scroll md:overflow-hidden">
-                    <div>
-                        <input
-                            className="px-3 py-1 bg-transparent text-black dark:text-white rounded-md border-[1px] border-outline lg:rounded-xl md:rounded-lg focus:outline-none"
-                            type="text"
-                            name="location"
+                    <div className="flex items-center gap-2 my-2 overflow-x-scroll md:overflow-hidden">
+                        <Input
                             value={location}
                             onChange={handleLocationChange}
-                            placeholder="enter city..."
-                        />
+                            placeHolder="enter city...."
+                        ></Input>
                         <Suggestions
                             data={locationData}
                             functionDrill={suggestionSelected}
                         ></Suggestions>
+                        <Button onClick={handleClick}>Clear</Button>
                     </div>
-                    <button
-                        onClick={handleClick}
-                        className="px-3 py-1 bg-transparent text-black dark:text-white rounded-md border-[1px] border-outline lg:rounded-xl md:rounded-lg focus:outline-none"
-                    >
-                        Clear
-                    </button>
+
+                    <div className="flex justify-between items-center font-info">
+                        <CurrentTemprature
+                            weatherData={weatherData}
+                        ></CurrentTemprature>
+                        <CurrentWeather
+                            weatherData={weatherData}
+                        ></CurrentWeather>
+                    </div>
+                    <Tabs defaultValue="temprature" className="flex-1">
+                        <TabsList>
+                            <TabsTrigger value="temprature">
+                                Temprature
+                            </TabsTrigger>
+                            <TabsTrigger value="precipatation">
+                                Precipitation
+                            </TabsTrigger>
+                            <TabsTrigger value="wind">Wind</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="temprature">
+                            <Component
+                                chartData={tempChartData(weatherData)}
+                                chartColor={"#3E5EFF"}
+                            ></Component>
+                        </TabsContent>
+                        <TabsContent value="precipatation">
+                            <Component
+                                chartData={presChartData(weatherData)}
+                                chartColor={"#E59061"}
+                            ></Component>
+                        </TabsContent>
+                        <TabsContent value="wind">
+                            <Component
+                                chartData={windChartData(weatherData)}
+                                chartColor={"#95B6F6"}
+                            ></Component>
+                        </TabsContent>
+                    </Tabs>
+                    <Daily weatherData={weatherData}></Daily>
+                </>
+            ) : (
+                <div className="flex items-center gap-2 my-2 overflow-x-scroll md:overflow-hidden">
+                    <Input
+                        value={location}
+                        onChange={handleLocationChange}
+                        placeHolder="enter city...."
+                    ></Input>
+                    <Suggestions
+                        data={locationData}
+                        functionDrill={suggestionSelected}
+                    ></Suggestions>
+                    <Button onClick={handleClick}>Clear</Button>
                 </div>
             )}
         </>
--- src/components/ui/DailyData.tsx
+++ src/components/ui/DailyData.tsx
@@ -21,35 +21,43 @@ export default function Daily({ weatherData }: { weatherData: apiData }) {
 
     return (
         <>
-            {dailyData().map((a) => {
-                return (
-                    <div
-                        key={a.day}
-                        className="flex flex-col min-w-max border-[1px] border-outline rounded-xl lg:rounded-3xl md:rounded-2xl justify-between items-center px-3 py-2"
-                    >
-                        <p className="text-lg text-black dark:text-white">
-                            {a.day.slice(0, 3)}
-                        </p>
-                        <img
-                            src={
-                                "/src/assets/weather-icons/" +
-                                weatherCodeConversion(a.weatherCode) +
-                                ".svg"
-                            }
-                            alt="There should be a icon here"
-                            className="size-12 mb-2"
-                        />
-                        <div>
-                            <p className="text-sm text-grayTxt leading-3">
-                                {Math.floor(a.maxTemp)}°
-                            </p>
-                            <p className="text-sm text-grayTxt">
-                                {Math.floor(a.minTemp)}°
-                            </p>
-                        </div>
+            <div className="overflow-scroll md:overflow-hidden">
+                <div className="flex md:justify-center">
+                    <div className="flex gap-2">
+                        {dailyData().map((a) => {
+                            return (
+                                <div
+                                    key={a.day}
+                                    className="flex flex-col min-w-max border-[1px] border-outline rounded-xl lg:rounded-3xl md:rounded-2xl justify-between items-center px-3 py-2"
+                                >
+                                    <p className="text-lg text-black dark:text-white">
+                                        {a.day.slice(0, 3)}
+                                    </p>
+                                    <img
+                                        src={
+                                            "/src/assets/weather-icons/" +
+                                            weatherCodeConversion(
+                                                a.weatherCode
+                                            ) +
+                                            ".svg"
+                                        }
+                                        alt="There should be a icon here"
+                                        className="size-12 mb-2"
+                                    />
+                                    <div>
+                                        <p className="text-sm text-grayTxt leading-3">
+                                            {Math.floor(a.maxTemp)}°
+                                        </p>
+                                        <p className="text-sm text-grayTxt">
+                                            {Math.floor(a.minTemp)}°
+                                        </p>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
-                );
-            })}
+                </div>
+            </div>
         </>
     );
 }
